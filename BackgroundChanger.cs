abaa417be6d146b7b3eb796eab417639ca09a807
LABOURE ROMAIN <rlaboure@tibco.fr>
2021-01-21 12:57:50 +0100

    first commit

---
+++ BackgroundChanger.cs
@@ -0,0 +1,76 @@
+﻿using Playnite.SDK;
+using Playnite.SDK.Models;
+using Playnite.SDK.Plugins;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace BackgroundChanger
+{
+    public class BackgroundChanger : Plugin
+    {
+        private static readonly ILogger logger = LogManager.GetLogger();
+
+        private BackgroundChangerSettings settings { get; set; }
+
+        public override Guid Id { get; } = Guid.Parse("3afdd02b-db6c-4b60-8faa-2971d6dfad2a");
+
+        public BackgroundChanger(IPlayniteAPI api) : base(api)
+        {
+            settings = new BackgroundChangerSettings(this);
+        }
+
+        public override void OnGameInstalled(Game game)
+        {
+            // Add code to be executed when game is finished installing.
+        }
+
+        public override void OnGameStarted(Game game)
+        {
+            // Add code to be executed when game is started running.
+        }
+
+        public override void OnGameStarting(Game game)
+        {
+            // Add code to be executed when game is preparing to be started.
+        }
+
+        public override void OnGameStopped(Game game, long elapsedSeconds)
+        {
+            // Add code to be executed when game is preparing to be started.
+        }
+
+        public override void OnGameUninstalled(Game game)
+        {
+            // Add code to be executed when game is uninstalled.
+        }
+
+        public override void OnApplicationStarted()
+        {
+            // Add code to be executed when Playnite is initialized.
+        }
+
+        public override void OnApplicationStopped()
+        {
+            // Add code to be executed when Playnite is shutting down.
+        }
+
+        public override void OnLibraryUpdated()
+        {
+            // Add code to be executed when library is updated.
+        }
+
+        public override ISettings GetSettings(bool firstRunSettings)
+        {
+            return settings;
+        }
+
+        public override UserControl GetSettingsView(bool firstRunSettings)
+        {
+            return new BackgroundChangerSettingsView();
+        }
+    }
+}
