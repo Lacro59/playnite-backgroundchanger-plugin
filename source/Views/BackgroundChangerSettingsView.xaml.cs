47d2fb003a11673a162269e59790688fb3a7ed33
Lacro59 <lacro59@gmail.com>
2025-06-09 15:43:32 +0200

    Would it be possible to change the minimum interval for background image switching to 5 seconds? #53

--- source/Views/BackgroundChangerSettingsView.xaml.cs
+++ source/Views/BackgroundChangerSettingsView.xaml.cs
@@ -13,40 +13,7 @@ namespace BackgroundChanger.Views
         public BackgroundChangerSettingsView()
         {
             InitializeComponent();
-
-            HwBcSlider_ValueChanged(hwSlider, null);
-            HwSlider_ValueChanged(hwSlider, null);
         }
-
-
-        private void HwBcSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
-        {
-            if (sender == null)
-            {
-                return;
-            }
-
-            Slider slider = sender as Slider;
-            if (labelBcIntervalLabel_text?.Content != null)
-            {
-                labelBcIntervalLabel_text.Content = "(" + slider.Value + " " + ResourceProvider.GetString("LOCBcSeconds") + ")";
-            }
-        }
-
-        private void HwSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
-        {
-            if (sender == null)
-            {
-                return;
-            }
-
-            Slider slider = sender as Slider;
-            if (labelIntervalLabel_text?.Content != null)
-            {
-                labelIntervalLabel_text.Content = "(" + slider.Value + " " + ResourceProvider.GetString("LOCBcSeconds") + ")";
-            }
-        }
-
 
         private void ButtonFfmpeg_Click(object sender, RoutedEventArgs e)
         {
@@ -54,10 +21,9 @@ namespace BackgroundChanger.Views
             if (!selectedFile.IsNullOrEmpty())
             {
                 PART_FfmpegFile.Text = selectedFile;
-                ((BackgroundChangerSettingsViewModel)this.DataContext).Settings.ffmpegFile = selectedFile;
+                ((BackgroundChangerSettingsViewModel)DataContext).Settings.ffmpegFile = selectedFile;
             }
         }
-
 
         private void ButtonWebpinfo_Click(object sender, RoutedEventArgs e)
         {
@@ -65,35 +31,7 @@ namespace BackgroundChanger.Views
             if (!selectedFile.IsNullOrEmpty())
             {
                 PART_WebpinfoFile.Text = selectedFile;
-                ((BackgroundChangerSettingsViewModel)this.DataContext).Settings.webpinfoFile = selectedFile;
-            }
-        }
-
-        private void HwBcVideoSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
-        {
-            if (sender == null)
-            {
-                return;
-            }
-
-            Slider slider = sender as Slider;
-            if (labelBcVideoIntervalLabel_text?.Content != null)
-            {
-                labelBcVideoIntervalLabel_text.Content = "(" + slider.Value + " " + ResourceProvider.GetString("LOCBcSeconds") + ")";
-            }
-        }
-
-        private void HwVideoSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
-        {
-            if (sender == null)
-            {
-                return;
-            }
-
-            Slider slider = sender as Slider;
-            if (labelVideoIntervalLabel_text?.Content != null)
-            {
-                labelVideoIntervalLabel_text.Content = "(" + slider.Value + " " + ResourceProvider.GetString("LOCBcSeconds") + ")";
+                ((BackgroundChangerSettingsViewModel)DataContext).Settings.webpinfoFile = selectedFile;
             }
         }
     }
