d1e43223393bd26a5a1577aabd016dfb2e557f56
Lacro59 <lacro59@gmail.com>
2025-06-09 13:07:05 +0200

    fix when missing file

--- source/Models/GameBackgroundImages.cs
+++ source/Models/GameBackgroundImages.cs
@@ -8,16 +8,16 @@ namespace BackgroundChanger.Models
     public class GameBackgroundImages : PluginDataBaseGame<ItemImage>
     {
         [DontSerialize]
-        public bool HasDataBackground => Items?.Where(x => !x.IsCover)?.Count() > 0;
+        public bool HasDataBackground => Items?.Where(x => !x.IsCover && x.Exist)?.Count() > 0;
 
         [DontSerialize]
-        public List<ItemImage> ItemsBackground => Items?.Where(x => !x.IsCover)?.ToList();
+        public List<ItemImage> ItemsBackground => Items?.Where(x => !x.IsCover && x.Exist)?.ToList();
 
 
         [DontSerialize]
-        public bool HasDataCover => Items?.Where(x => x.IsCover)?.Count() > 0;
+        public bool HasDataCover => Items?.Where(x => x.IsCover && x.Exist)?.Count() > 0;
 
         [DontSerialize]
-        public List<ItemImage> ItemsCover => Items?.Where(x => x.IsCover)?.ToList();
+        public List<ItemImage> ItemsCover => Items?.Where(x => x.IsCover && x.Exist)?.ToList();
     }
 }
